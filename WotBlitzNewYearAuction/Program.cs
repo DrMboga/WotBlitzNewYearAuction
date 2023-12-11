c3b6075943512482d6ac46774a665a3f1f024674
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-11 16:38:13 +0100

    Feat: Initial console app

---
+++ WotBlitzNewYearAuction/Program.cs
@@ -0,0 +1,6 @@
+﻿// TankName | Tier-Nation-Type | Price | Left
+
+// https://eu.wotblitz.com/en/api/events/items/auction/?type[]=vehicle&saleable=true
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
