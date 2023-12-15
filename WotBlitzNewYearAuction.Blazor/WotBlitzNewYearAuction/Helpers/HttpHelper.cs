0decd90dc865335d052c14da1282d8d2d204c908
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-15 12:41:51 +0100

    chore: try to request http

--- WotBlitzNewYearAuction.Blazor/WotBlitzNewYearAuction/Helpers/HttpHelper.cs
+++ WotBlitzNewYearAuction.Blazor/WotBlitzNewYearAuction/Helpers/HttpHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
+using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 namespace WotBlitzNewYearAuction;
 
@@ -8,14 +9,18 @@ public static class HttpHelper
 {
     const string AuctionApiUrl = "https://eu.wotblitz.com/en/api/events/items/auction/?page_size=80&type[]=vehicle&saleable=true";
 
-    public static async Task<AuctionResponse?> GetAuctionData() {
-        using HttpClient client = new();
+    public static async Task<AuctionResponse?> GetAuctionData(this HttpClient client) {
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
         client.DefaultRequestHeaders.Add("User-Agent", ".NET Auction app");
 
-        await using Stream stream = await client.GetStreamAsync(AuctionApiUrl);
+        var request = new HttpRequestMessage(HttpMethod.Get, AuctionApiUrl);
+        request.SetBrowserRequestMode(BrowserRequestMode.NoCors);
+        
+        var httpResponse = await client.SendAsync(request);
+        httpResponse.EnsureSuccessStatusCode();
+        await using Stream stream = await httpResponse.Content.ReadAsStreamAsync();
         var response = await JsonSerializer.DeserializeAsync<AuctionResponse>(stream);
 
         return response;
