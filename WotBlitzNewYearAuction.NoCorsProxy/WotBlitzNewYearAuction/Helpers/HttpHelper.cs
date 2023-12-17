dff0bcf78949a4640700a4b29b75e4b76f8e0172
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-17 15:13:00 +0100

    chore: no cors proxy optimized

--- WotBlitzNewYearAuction.NoCorsProxy/WotBlitzNewYearAuction/Helpers/HttpHelper.cs
+++ WotBlitzNewYearAuction.NoCorsProxy/WotBlitzNewYearAuction/Helpers/HttpHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System.Net.Http.Headers;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace WotBlitzNewYearAuction;
 
@@ -8,14 +9,14 @@ public static class HttpHelper
 {
     const string AuctionApiUrl = "https://eu.wotblitz.com/en/api/events/items/auction/?page_size=80&type[]=vehicle&saleable=true";
 
-    public static async Task<AuctionResponse?> GetAuctionData(this HttpClient client) {
+    public static async Task<JsonObject?> GetAuctionData(this HttpClient client) {
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(
             new MediaTypeWithQualityHeaderValue("application/json"));
         client.DefaultRequestHeaders.Add("User-Agent", ".NET Auction app");
 
         await using Stream stream = await client.GetStreamAsync(AuctionApiUrl);
-        var response = await JsonSerializer.DeserializeAsync<AuctionResponse>(stream);
+        var response = await JsonSerializer.DeserializeAsync<JsonObject>(stream);
 
         return response;
     }
