ff6ea157a4fd1b5d5d6c69c327e26d44ebcba56f
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-18 18:13:28 +0100

    feat: React persist initial prices

--- WotBlitzNewYearAuction.React/frontend/src/Components/UseAuction.tsx
+++ WotBlitzNewYearAuction.React/frontend/src/Components/UseAuction.tsx
@@ -1,13 +1,24 @@
 // @flow
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { auctionAPI } from '../api/AuctionApi';
 import { AuctionItem } from '../Model/AuctionItem';
+import { InitialPrices } from '../Model/InitialPrices';
+
+const LocalStorageKey = 'initial-prices';
 
 export function useAuction() {
   const [loading, setLoading] = React.useState(false);
   const [auctionItems, setAuctionItems] = React.useState<AuctionItem[]>([]);
   const [error, setError] = React.useState(null);
+
+  const [initialPrices, setInitialPrices] = useState<InitialPrices[]>(
+    JSON.parse(localStorage.getItem(LocalStorageKey) ?? '[]') ?? [],
+  );
+
+  useEffect(() => {
+    localStorage.setItem(LocalStorageKey, JSON.stringify(initialPrices));
+  }, [initialPrices]);
 
   useEffect(() => {
     setLoading(true);
@@ -16,6 +27,27 @@ export function useAuction() {
     auctionAPI
       .get()
       .then((data) => {
+        if (data?.results) {
+          const initialPricesCopy = [...initialPrices];
+          for (const auctionItem of data.results.filter(
+            (i) => i.available && i.current_count > 0,
+          )) {
+            const initialPrice = initialPricesCopy.find(
+              (i) => i.vehicleId === auctionItem.entity!.id,
+            );
+            if (initialPrice) {
+              auctionItem.initialPrice = initialPrice.initialPrice;
+            } else {
+              auctionItem.initialPrice = auctionItem.price!.value;
+              initialPricesCopy.push({
+                vehicleId: auctionItem.entity!.id,
+                initialPrice: auctionItem.price!.value,
+              });
+            }
+          }
+          setInitialPrices(initialPricesCopy);
+        }
+
         setAuctionItems(data?.results ?? []);
         setLoading(false);
       })
