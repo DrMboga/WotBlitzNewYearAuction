39a89938e5994b0fb5e86e505658335893f04041
Mikhail Shabanov <dr.mboga@yahoo.com>
2023-12-20 12:02:40 +0100

    feat: angular initial app

---
+++ WotBlitzNewYearAuction.Angular/WotBlitzNewYearAuction/src/app/app.routes.ts
@@ -0,0 +1,4 @@
+import { Routes } from '@angular/router';
+import { AuctionComponent } from './Auction/Components/auction/auction.component';
+
+export const routes: Routes = [{ path: '', component: AuctionComponent }];
